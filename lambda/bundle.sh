0666db901fc819e3ee019d16a65235a12ba332b2
Michael Wittig <post@michaelwittig.info>
2015-10-06 12:19:32 -0700

    added lambda code

---
+++ lambda/bundle.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+zip -r lambda.zip *.js
---
+++ lambda/githubPOST.js
@@ -0,0 +1,6 @@
+console.log('Loading function');
+
+exports.handler = function(event, context) {
+	console.log('Received event:', JSON.stringify(event, null, 2));
+	context.succeed('pong');
+};
