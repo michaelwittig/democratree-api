3f8821708e0a1631bc885e76cd8a658de2d165c7
Michael Wittig <post@michaelwittig.info>
2015-10-06 18:06:17 -0700

    react to /github/{token}

--- lambda/githubPOST.js
+++ lambda/githubPOST.js
@@ -1,9 +1,12 @@
 console.log('loading function');
 var aws = require('aws-sdk');
+var crypto = require('crypto');
+
 var sqs = new aws.SQS();
 var dynamodb = new aws.DynamoDB();
 
 function send(message, context) {
+  console.log("send:", JSON.stringify(message, null, 2));
   sqs.sendMessage({
     "MessageBody": JSON.stringify(message),
     "QueueUrl": "https://sqs.us-east-1.amazonaws.com/089545019273/democratree-worker"
@@ -16,31 +19,44 @@ function send(message, context) {
   });
 }
 
+function calcDigest(key, blob) {
+  return 'sha1=' + crypto.createHmac('sha1', key).update(blob).digest('hex');
+}
+
 exports.handler = function(event, context) {
-	console.log('received event:', JSON.stringify(event, null, 2));
-  if (event.ref_type === 'tag' && event.pusher !== undefined) {
+  console.log('received event:', JSON.stringify(event)); // TODO debug only
+  var body = event.body;
+  var header = event.header;
+  var hook = event.hook;
+  var digest = calcDigest("test1234", JSON.stringify(body));
+  if (header['X-Hub-Signature'] !== digest) {
+    //context.fail(new Error("digest wrong"));
+    //return;
+    console.log("digest wrong"); // TODO flip to other implementation
+  }
+  if (header['X-Github-Event'] === 'create' && body.ref_type === 'tag' && body.pusher !== undefined) {
     send({
       "action": "create_version",
-      "version": event.ref.substr(10),
+      "version": body.ref.substr(10),
       "repository": {
-        "full_name": event.repository.full_name
+        "full_name": body.repository.full_name
       },
       "user": {
-        "email": event.pusher.email
+        "email": body.pusher.email
       }
     }, context);
-  } else if (event.repository !== undefined && event.pusher !== undefined && event.action === undefined) {
+  } else if (header['X-Github-Event'] === 'push' && body.repository !== undefined && body.pusher !== undefined) {
     send({
       "action": "create_or_update",
       "repository": {
-        "full_name": event.repository.full_name
+        "full_name": body.repository.full_name
       },
       "user": {
-        "email": event.pusher.email
+        "email": body.pusher.email
       }
     }, context);
   } else {
-    console.log('I only support push events');
+    console.log('I only support certain events, but not:', JSON.stringify(event, null, 2));
     context.succeed();
   }
 };
